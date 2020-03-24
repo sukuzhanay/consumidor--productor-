0f536d4616d2bcbbabadf8a3ae86d723373f6a2d
N3oZ3r0 <apocaliptico@gmail.com>
2020-03-24 16:57:07 +0100

    Done

---
+++ Producer.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class Producer extends Thread {
+    public Producer(){
+        start();
+    }
+
+    private void produce(){
+
+        Random rdmNum = new Random();
+        int numP = rdmNum.nextInt(999) + 1;
+        int sleepTime = rdmNum.nextInt(250 - 25 + 1) + 25;
+
+        try {
+            sleep(sleepTime);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Producto Numero " + numP + "prducido.");
+
+        Buffer.getStore().add(numP);
+    }
+
+    public void run(){
+
+        while(true){
+            if (Buffer.getStore().size() == Buffer.bSize){
+                System.out.println("Productor: El buffer esta lleno, esperando a que el consumidor trabaje.");
+            }
+            try{
+                Buffer.getsNoLleno().acquire();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+
+            produce();
+
+            Buffer.getsNoVacio().release();
+        }
+    }
+}
---
+++ Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+        new Producer();
+        new Consumer();
+
+    }
+}
